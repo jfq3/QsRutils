07b1805e9bb4bad526bf86b7a83926428f9ef8db
John Quensen <quensenj@msu.edu>
2025-10-11 18:08:31 -0400

    modified get_groups, hash_dna_seqs, location of fastq files

--- R/hash_dna_seqs.R
+++ R/hash_dna_seqs.R
@@ -2,7 +2,7 @@
 #' @name hash_dna_seqs
 #' @description Renames sequences in a multi-fasta file with the MD5 hash of each sequence.
 #' @param seqs A list of DNA sequences
-#' 
+#' @importFrom insect hash
 #' @returns DNA sequences renamed with their hashes.
 #' @details Taxa names for the ASV table returned by the R version of DADA2 are the sequences themselves. This function renames them with the MD5 hash of each sequences so that the results are directly comparable QIIME2/DADA2 results.
 #' @export
