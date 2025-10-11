542f64238c8c5e2bf90cb52610f62acfb61a943a
John Quensen <quensenj@msu.edu>
2025-10-11 11:39:27 -0400

    Add x without y

--- R/without.R
+++ R/without.R
@@ -1,1 +1,18 @@
-"%w/o%" <- function(x, y) x[!x %in% y] #--  x without y
+#' @title Remove elements from a vector
+#'
+#' @description This infix operator removes all elements of `y` from `x`.
+#'
+#' @param x A vector.
+#' @param y A vector of elements to remove from `x`.
+#'
+#' @return A vector containing elements from `x` that are not present in `y`.
+#'
+#' @examples
+#' c(1, 2, 3, 4, 5) %w/o% c(2, 4)
+#' letters[1:5] %w/o% c("a", "c")
+#'
+#' @export
+`%w/o%` <- function(x, y) {
+  x[!x %in% y]
+}
+
