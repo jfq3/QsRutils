58312774d38fd5ce28b906465d165ea04909ca42
John Quensen <quensenj@msu.edu>
2025-10-13 08:39:47 -0400

    Add example to check_varariance

--- R/check_variance.R
+++ R/check_variance.R
@@ -2,7 +2,7 @@
 #'
 #' Tests for Heterogeneity of Variances in make_comparisons Result
 #'
-#' @param otu.pc.transformed An OTU matrix of transformed data.
+#' @param otu.pc.transformed An OTU matrix of transformed data. Taxa are rows.
 #' @param group.vector A vector of treatments.
 #'
 #' @return Prints test results to the console.
@@ -13,9 +13,15 @@
 #' @seealso make_comparisons
 #'
 #' @examples
-#' \dontrun{
-#' 
-#' }
+#' # Transform species matrix to proportion;
+#' # Check variances for the first three species
+#' # in the dune data set grouped by Management.
+#' data(dune)
+#' data(dune.env)
+#' dune <- vegan::decostand(dune, method = "total")
+#' dune <- dune[, 1:3]
+#' dune <- t(dune)
+#' check_var(dune, dune.env$Management)
 #'
 check_var <- function(otu.pc.transformed, group.vector) {
   for (i in 1:nrow(otu.pc.transformed)) {
