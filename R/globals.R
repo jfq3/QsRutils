987e37d2d28f74fb18d71c726db92e65d491186e
John Quensen <quensenj@msu.edu>
2025-12-26 09:54:44 -0500

    Combined definition of global variables in one file.

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,5 @@
+# Prevent RCMD CHECK from reporting no visible binding for 
+# variables created dynamically
+
+utils::globalVariables("Max")
 utils::globalVariables(c("seq_length", "taxon"))
--- R/zzz.R
+++
@@ -1,4 +0,0 @@
-# Prevent RCMD CHECK from reporting no visible binding for 
-# variables created dynamically
-
-utils::globalVariables("Max")
