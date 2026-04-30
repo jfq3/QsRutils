e4d2333fd9d56636d0f92c6cefd77518687ddfb7
John Quensen <quensenj@msu.edu>
2026-04-30 09:44:45 -0400

    Fix check_variance regarding CRAN comments

--- R/check_variance.R
+++ R/check_variance.R
@@ -5,7 +5,9 @@
 #' @param otu.pc.transformed An OTU matrix of transformed data. Taxa are rows.
 #' @param group.vector A vector of treatments.
 #'
-#' @return Prints test results to the console.
+#' @return A data frame of class \code{"check_var"} with one row per taxon and
+#'   columns \code{taxon}, \code{statistic}, \code{df}, and \code{p.value} from
+#'   the Fligner-Killeen test. The object can be printed with \code{print()}.
 #'
 #' @export
 #'
@@ -24,9 +26,25 @@
 #' check_var(dune, dune.env$Management)
 #'
 check_var <- function(otu.pc.transformed, group.vector) {
-  for (i in 1:nrow(otu.pc.transformed)) {
-    rslt <- stats::fligner.test(otu.pc.transformed[ i, ], g = group.vector)
-    print((rownames(otu.pc.transformed))[i])
-    print(rslt)
-  }
+  results <- lapply(seq_len(nrow(otu.pc.transformed)), function(i) {
+    rslt <- stats::fligner.test(otu.pc.transformed[i, ], g = group.vector)
+    data.frame(
+      taxon     = rownames(otu.pc.transformed)[i],
+      statistic = unname(rslt$statistic),
+      df        = unname(rslt$parameter),
+      p.value   = rslt$p.value,
+      row.names = NULL
+    )
+  })
+  out <- do.call(rbind, results)
+  class(out) <- c("check_var", "data.frame")
+  out
+}
+
+#' @export
+#' @rdname check_var
+print.check_var <- function(x, ...) {
+  cat("Fligner-Killeen Test of Homogeneity of Variances\n\n")
+  print(data.frame(x), ...)
+  invisible(x)
 }
