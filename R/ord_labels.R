db1cd68a03ed5fac6c9aab8d1eb7e1d7b62a0cbf
John Quensen <quensenj@msu.edu>
2026-05-01 16:39:51 -0400

    Add examples to ord_labels()

--- R/ord_labels.R
+++ R/ord_labels.R
@@ -1,23 +1,56 @@
-#' Make Ordination Axis Labels
+#'Make Ordination Axis Labels
 #'
-#' Makes ordination axis labels that include, if appropriate, the \% total variance explained by each axis.
+#'Makes ordination axis labels that include, if appropriate, the % of the total
+#'variance explained by each axis.
+#'
+#'@usage ord_labels(ord)
+#'
+#'@param ord A vegan ordination object.
+#'
+#'@return A character vector, each element of which can be used to label the
+#'  corresponding axis of an ordination plot.
+#'
+#'@details 
+#'  If there are no eigenvalues in ord, or if any eigenvalues are less than 0,
+#'  each element of the vector returned has the form "DIMn" where n is the axis
+#'  number. Otherwise, each element of the vector returned has the form Axisn
+#'  xx.x % where Axis is taken from the vector of eigenvalues in ord if they
+#'  are named or simply "DIM" if they are not, n is the number of the axis, and
+#'  xx.x is the % of total variance explained by the axis.
+#'  
+#'  For this function to work correctly, ord should be created in one of
+#'  the following ways: 
+#'  1. As an unconstrained ordination using vegan::rda. In
+#'  this case the labels have the form PCAn xx.x % 
+#'  2. As a PCoA made with stats::cmdscale. In this case the labels have the form DIMn xx.x %.
+#'  3. As a CA made with vegan::ca. In this case the labels have the form CAn xx.x %.
+#'  
+#'@md
+#'@seealso [rda_labels()]
+#'@export
+#'
+#'@examples
+#'{
+#' # For PCA using rda:
+#' data("dune", package = "vegan")
+#' dune_hel <- vegan::decostand(dune, method = "hellinger")
+#' pca <- vegan::rda(dune_hel)
+#' ord_labels(pca)[1:2]
 #' 
-#' @usage ord_labels(ord)
+#' # For PCoA with negative eigenvalues
+#' d <- vegan::vegdist(dune)
+#' pcoa <- stats::cmdscale(d, k = nrow(dune)-1, eig = TRUE, add = FALSE)
+#' ord_labels(pcoa)[1:2]
 #' 
-#' @param ord A vegan ordination object.
-#'
-#' @return A character vector, each element of which can be used to label the corresponding axis of an ordination plot.
-#'
-#' @details If there are no eigenvalues in ord, or if any eigenvalues are less than 0, each element of the vector returned has the form "DIMn" where N is the axis number. Otherwise, each element of the vector returned has the form "AxisN xx.x\%" where "Axis" is taken from the vector of eigenvalues in ord if they are named or simply "DIM" if they are not, N is the number of the axis, and xx.x is the \% of total variance explained by the axis.
-#'
-#' @export
-#'
-#' @examples
-#'\dontrun{
-#' ord_labels(nmds_ordination)
-#' ord_labels(pca_ordination
-#' ord_labels(pcoa_ordination))
+#' # For PCoA without negative eigenvalues
+#' pcoa <- stats::cmdscale(d, k = nrow(dune)-1, eig = TRUE, add = TRUE)
+#' ord_labels(pcoa)[1:2]
+#' 
+#' # For correspondence analysis
+#' ca_ord <- vegan::ca(dune)
+#' ord_labels(ca_ord)
 #'}
+
 ord_labels <-
   function(ord){
     ev <- vegan::eigenvals(ord)
