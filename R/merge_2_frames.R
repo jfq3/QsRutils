454c677eeb8eaea56d2697ea0dac4b7f38c70eba
John Quensen <quensenj@msu.edu>
2026-05-02 11:20:49 -0400

    Add example to merge_2_frames()

--- R/merge_2_frames.R
+++ R/merge_2_frames.R
@@ -9,8 +9,22 @@
 #' @export
 #' @details Merges data frames by common row names. This function differs from  merge.data.frames in that the merged data frame returned has row names and not a new column of the row names.
 #' @examples
-#' \dontrun{
-#' merge_2_frames(dataframe_1, dataframe_2)
+#' {
+#' common_rows <- paste0("ID_", 1:5)
+#'
+#' df1 <- data.frame(
+#'   Value_A = runif(5), 
+#'   Category = sample(c("X", "Y"), 5, replace = TRUE),
+#'   row.names = common_rows
+#' )
+#' 
+#' df2 <- data.frame(
+#'   Value_B = rnorm(5),
+#'   Flag = sample(c(TRUE, FALSE), 5, replace = TRUE),
+#'   row.names = common_rows
+#' )
+#' 
+#' merge_2_frames(df1, df2)
 #' }
 #'
 merge_2_frames <- function(one, two) {
