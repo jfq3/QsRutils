821e2482f01379a93b2746faa2d3a26160c530cb
John Quensen <quensenj@msu.edu>
2026-05-02 13:52:44 -0400

    Moved its.root and plot_df into data.R. Previouslyin separate files that have been deleted.

--- R/data.R
+++ R/data.R
@@ -1,3 +1,15 @@
+#' An Experiment Level phyloseq Object
+#'
+#' Based on ITS2 sequences amplified from corn roots.
+#'
+#' @format A phyloseq object with otu_table, sample_data and tax_table. The sample_data variables are:
+#' \describe{
+#'   \item{P}{Phosporous level, H or L}
+#'   \item{Genotype}{ One of three: 2, 3, and C}
+#'   \item{Label}{A code for treatments: 2HR, 2LR, 3HR, 3LR, CHR, CLR}
+#' }
+"its.root"
+
 #' A Data File in Long Format
 #'
 #' Used in Case 3 of the vignette make_comparisons
@@ -7,9 +19,7 @@
 #'   \item{Treatment}{A code for genotype (2, 3, or C), P level (H or L) and sample type (R)}
 #'   \item{Family}{One of the families in Gigasporaceae}
 #'   \item{Percent}{Percent of total counts for family and treatment combination.}
-#'
-#'  }
-#'
-#'
-#'
+#' }
 "plot_df"
+
+
