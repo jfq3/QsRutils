127006fbef88cb8627a76abf9755211667b9df46
John Quensen <quensenj@msu.edu>
2026-05-08 11:18:01 -0400

    Added details to the function get_plot_limits().

--- R/get_plot_limits.R
+++ R/get_plot_limits.R
@@ -4,7 +4,10 @@
 #' 
 #' @param plot A plot created with ggplot2
 #'
-#' @return A list: xmin, xmax, ymin, ymax
+#' @return A list: xmin, xmax, ymin, ymax giving the coordinates of the limits of a ggplot panel.
+#' @details
+#' Sometimes when adding text to a ggplot, the text is cutoff if it extends beyond the limits of plot panel. This function provides information enabling the user to extend the panel limits so that the text is not cutoff.
+#' 
 #' @export
 #' @examples
 #' library(ggplot2)
